133aa322354f643242137def7522c61e8736b3e7
Hamza-NAYA <hamza.naya@laplateforme.io>
2023-11-13 12:46:53 +0100

    plesk

---
+++ projet/realisation/swap.js
@@ -0,0 +1,8 @@
+function sectionswap() {
+    var section1 = document.getElementById('section1');
+    var section2 = document.getElementById('section2');
+
+    // Alterne la visibilité des sections
+    section1.classList.toggle('hidden');
+    section2.classList.toggle('hidden');
+}
